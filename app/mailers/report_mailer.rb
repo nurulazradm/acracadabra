81b9c04ec3c91d528e1d7886c6c0aa498f8f1c95
Jeremy Haberman <jeremy@livefront.com>
2012-10-08 11:17:38 -0500

    Add logging to ReportMailer

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -2,6 +2,8 @@ class ReportMailer < ActionMailer::Base
   default from: 'ACRAR <jeremy@livefront.com>'
   
   def report(email, report)
+    logger.info "email: " + email
+    logger.info report
     @params = report
     mail(to: email, subject: 'Android App Crash')
   end
