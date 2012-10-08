062c5076c626b4775db5a92abadb6584a59e2784
Jeremy Haberman <jeremy@livefront.com>
2012-10-08 07:05:30 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+gem 'pg', '0.14.1'
+
+group :development, :test do
+  gem 'rspec-rails', '2.11.0'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails', '2.1.3'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acrar::Application.config.secret_token = '74b2d35f417e687ccb784285551c8dc99e9706bbbd2eec36d96367b247a8e7af187b2816dbb7525b7e267be28f175f926dfcee4015b557a6def73c03b8206fa2'
