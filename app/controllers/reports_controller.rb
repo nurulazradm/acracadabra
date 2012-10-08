54e8a409cd8ddc27618b575c94cd4d6c656da5d7
Jeremy Haberman <jeremy@livefront.com>
2012-10-08 11:07:17 -0500

    Add logging to ReportsController

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,7 @@ class ReportsController < ApplicationController
   
   def create
     recipients = Rails.configuration.recipients
+    logger.info "Recipients: " + recipients
     ReportMailer.report(recipients, params)
     head :ok
   end
